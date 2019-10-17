66c10194c3a38767c6816a43cd759107f4d98492
LAdu3L <56685181+LAdu3L@users.noreply.github.com>
2019-10-17 16:39:27 +0300

    Add files via upload

---
+++ Gona1/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ButtonStart = New System.Windows.Forms.Button()
+        Me.ButtonSave = New System.Windows.Forms.Button()
+        Me.ButtonLoad = New System.Windows.Forms.Button()
+        Me.ListBoxSaves = New System.Windows.Forms.ListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LabelSTATUS = New System.Windows.Forms.Label()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.TextBoxSynapsis = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ButtonFiles = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ButtonStart
+        '
+        Me.ButtonStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.ButtonStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon
+        Me.ButtonStart.Location = New System.Drawing.Point(12, 130)
+        Me.ButtonStart.Name = "ButtonStart"
+        Me.ButtonStart.Size = New System.Drawing.Size(104, 30)
+        Me.ButtonStart.TabIndex = 0
+        Me.ButtonStart.Text = "Start"
+        Me.ButtonStart.UseVisualStyleBackColor = True
+        '
+        'ButtonSave
+        '
+        Me.ButtonSave.Location = New System.Drawing.Point(12, 169)
+        Me.ButtonSave.Name = "ButtonSave"
+        Me.ButtonSave.Size = New System.Drawing.Size(104, 30)
+        Me.ButtonSave.TabIndex = 1
+        Me.ButtonSave.Text = "Save"
+        Me.ButtonSave.UseVisualStyleBackColor = True
+        '
+        'ButtonLoad
+        '
+        Me.ButtonLoad.Location = New System.Drawing.Point(12, 208)
+        Me.ButtonLoad.Name = "ButtonLoad"
+        Me.ButtonLoad.Size = New System.Drawing.Size(104, 30)
+        Me.ButtonLoad.TabIndex = 2
+        Me.ButtonLoad.Text = "Load"
+        Me.ButtonLoad.UseVisualStyleBackColor = True
+        '
+        'ListBoxSaves
+        '
+        Me.ListBoxSaves.FormattingEnabled = True
+        Me.ListBoxSaves.Location = New System.Drawing.Point(138, 141)
+        Me.ListBoxSaves.Name = "ListBoxSaves"
+        Me.ListBoxSaves.Size = New System.Drawing.Size(79, 82)
+        Me.ListBoxSaves.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 8)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(222, 116)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'LabelSTATUS
+        '
+        Me.LabelSTATUS.AutoSize = True
+        Me.LabelSTATUS.Font = New System.Drawing.Font("Castellar", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelSTATUS.Location = New System.Drawing.Point(113, 386)
+        Me.LabelSTATUS.Name = "LabelSTATUS"
+        Me.LabelSTATUS.Size = New System.Drawing.Size(19, 19)
+        Me.LabelSTATUS.TabIndex = 5
+        Me.LabelSTATUS.Text = "-"
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Location = New System.Drawing.Point(147, 229)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(59, 20)
+        Me.ButtonDelete.TabIndex = 7
+        Me.ButtonDelete.Text = "Delete"
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'TextBoxSynapsis
+        '
+        Me.TextBoxSynapsis.Location = New System.Drawing.Point(51, 271)
+        Me.TextBoxSynapsis.Multiline = True
+        Me.TextBoxSynapsis.Name = "TextBoxSynapsis"
+        Me.TextBoxSynapsis.ReadOnly = True
+        Me.TextBoxSynapsis.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBoxSynapsis.Size = New System.Drawing.Size(140, 104)
+        Me.TextBoxSynapsis.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(88, 254)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Synapsis"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 8.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(151, 124)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Saves"
+        '
+        'ButtonFiles
+        '
+        Me.ButtonFiles.Location = New System.Drawing.Point(12, 244)
+        Me.ButtonFiles.Name = "ButtonFiles"
+        Me.ButtonFiles.Size = New System.Drawing.Size(44, 20)
+        Me.ButtonFiles.TabIndex = 11
+        Me.ButtonFiles.Text = "Files"
+        Me.ButtonFiles.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.PaleVioletRed
+        Me.ClientSize = New System.Drawing.Size(243, 414)
+        Me.Controls.Add(Me.ButtonFiles)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxSynapsis)
+        Me.Controls.Add(Me.ButtonDelete)
+        Me.Controls.Add(Me.LabelSTATUS)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ListBoxSaves)
+        Me.Controls.Add(Me.ButtonLoad)
+        Me.Controls.Add(Me.ButtonSave)
+        Me.Controls.Add(Me.ButtonStart)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ButtonStart As System.Windows.Forms.Button
+    Friend WithEvents ButtonSave As System.Windows.Forms.Button
+    Friend WithEvents ButtonLoad As System.Windows.Forms.Button
+    Friend WithEvents ListBoxSaves As System.Windows.Forms.ListBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents LabelSTATUS As System.Windows.Forms.Label
+    Friend WithEvents ButtonDelete As System.Windows.Forms.Button
+    Friend WithEvents TextBoxSynapsis As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ButtonFiles As System.Windows.Forms.Button
+
+End Class
---
+++ Gona1/Form1.vb
@@ -0,0 +1,286 @@
+﻿Imports System
+Imports Microsoft
+Imports System.IO
+Imports Microsoft.VisualBasic
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Dim runsN As Integer
+    Dim selectedSave As Integer
+    Dim userSynapsis As String = Nothing
+    Dim savedSynapsis As String
+    Dim savedSynapsisText As String
+    Dim saveToDelete As Integer
+    Dim saveSynapsisDeleteCandidate As String
+    Dim theWholefile As String
+    Dim SaveFile As System.IO.StreamWriter
+    Dim shortCutName As String
+    Dim dialog As New FolderBrowserDialog
+    Dim TargetName As String
+    Dim ShortCutPath As String
+    Dim oShell As Object
+    Dim oLink As Object
+    'pathlnk
+    Dim pathNoita_Runs As String = System.Windows.Forms.Application.StartupPath & "\Noita_Runs"
+    Dim pathNoita_RunsFolder As String = System.Windows.Forms.Application.StartupPath & "\Noita_Runs\"
+    Dim pathNoitaS As String = ""
+    Dim pathlnkynapsisText As String = System.Windows.Forms.Application.StartupPath & "\SaveSynapsis.txt"
+    Dim pathsave00 As String = ""
+    Dim appPath As String = System.Windows.Forms.Application.StartupPath
+
+    'DEBUG
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles Me.Click
+    End Sub
+
+    'FORM LOAD
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'check if files exist containing paths. If not get paths and create them
+        CheckPathTxt()
+
+        'Get the latest save and the number of saves
+        checkSaves()
+    End Sub
+    Private Sub checkSaves()
+        'Clear listbox
+        ListBoxSaves.Items.Clear()
+        'Get the latest save and the number of saves
+        For Each Dir As String In Directory.GetDirectories(pathNoita_Runs)
+            runsN = My.Computer.FileSystem.GetName(Dir)
+            ListBoxSaves.Items.Add(My.Computer.FileSystem.GetName(Dir))
+        Next
+    End Sub
+    'check paths .txt files
+    Private Sub CheckPathTxt()
+
+        'TESTS:
+
+        'Noita_Runs(folder)
+        If (Not System.IO.Directory.Exists(appPath & "\Noita_Runs")) Then
+            System.IO.Directory.CreateDirectory(appPath & "\Noita_Runs")
+        End If
+
+        'noitaS.lnk
+
+        If My.Computer.FileSystem.FileExists("pathlnk.txt") = True Then
+            'get the path from file
+            pathNoitaS = My.Computer.FileSystem.ReadAllText("pathlnk.txt")
+            'delete the stupid space that saving the file creates
+            pathNoitaS = pathNoitaS.Replace(vbLf, "").Replace(vbCr, "")
+        Else
+            'If not then ask a path to it create a shortcut for launching
+            MsgBox("Navigate and select the Noita game folder" & vbNewLine & "Usually:" & vbNewLine & "C:-->Program Files(x86)-->steam-->steamapps-->common-->Noita")
+            CreateShortCut()
+            pathNoitaS = ShortCutPath & "\noitaS.lnk"
+        End If
+
+        'Noita saves (saves00)
+
+        If My.Computer.FileSystem.FileExists("pathSave00.txt") = True Then
+            'get the path from file
+            pathsave00 = My.Computer.FileSystem.ReadAllText("pathSave00.txt")
+            'delete the stupid space that saving the file creates
+            pathsave00 = pathsave00.Replace(vbLf, "").Replace(vbCr, "")
+        Else
+            'If not then ask a path 
+            MsgBox("Navigate and select the Noita save folder" & vbNewLine & "Usually:" & "C:-->User-->'User'-->AppData-->LocalLow-->Nolla_Games_Noita-->save00")
+            If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                pathsave00 = dialog.SelectedPath
+                'create a .txt file that stores the path to save00 
+                SaveFile = My.Computer.FileSystem.OpenTextFileWriter(appPath & "\pathSave00.txt", True)
+                SaveFile.WriteLine(pathsave00)
+                SaveFile.Close()
+            End If
+        End If
+
+
+    End Sub
+
+    'START
+    Private Sub ButtonStart_Click(sender As Object, e As EventArgs) Handles ButtonStart.Click
+        'read path and run the shortcut
+        Process.Start(pathNoitaS)
+        'update labels
+        LabelSTATUS.Text = "Starting..."
+        LabelSTATUS.Left = Me.Width / 2 - LabelSTATUS.Width / 2
+
+    End Sub
+
+    'SAVE
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles ButtonSave.Click
+        'create a new save folder 
+        My.Computer.FileSystem.CreateDirectory(pathNoita_RunsFolder & runsN + 1)
+        'Increment var 
+        runsN += 1
+        'Ask user for a save explanation
+
+
+
+        userSynapsis = InputBox("Give a short synapsis of this save", "A Short Synopsis", " -")
+
+        SaveFile = My.Computer.FileSystem.OpenTextFileWriter(pathlnkynapsisText, True)
+        SaveFile.WriteLine(runsN & userSynapsis)
+        SaveFile.Close()
+
+
+        'copy the files to the new save folder
+        For Each file As String In IO.Directory.GetFiles(pathsave00)
+            IO.File.Copy(file, file.Replace(pathsave00, pathNoita_RunsFolder & runsN))
+        Next
+        'Update listbox
+        checkSaves()
+        LabelSTATUS.Text = "Saved!"
+        LabelSTATUS.Left = Me.Width / 2 - LabelSTATUS.Width / 2
+    End Sub
+
+    'LOAD
+    Private Sub ButtonLoad_Click(sender As Object, e As EventArgs) Handles ButtonLoad.Click
+        'Check if user has selected a save to load
+        If selectedSave = Nothing Then
+            MsgBox("Select a SAVE to load")
+        Else
+            'delete current game save
+            If My.Computer.FileSystem.FileExists(pathsave00 & "\magic_numbers.salakieli") = True Then
+                FileSystem.Kill(pathsave00 & "\*.*")
+            Else
+            End If
+            'copy the save to the game
+            My.Computer.FileSystem.CopyDirectory(pathNoita_RunsFolder & selectedSave, pathsave00, True)
+        End If
+        'start the game
+        Process.Start(pathNoitaS)
+        LabelSTATUS.Text = "Starting..."
+        LabelSTATUS.Left = Me.Width / 2 - LabelSTATUS.Width / 2
+    End Sub
+
+    'SELECT SAVE
+    Private Sub ListBoxSaves_Click(sender As Object, e As EventArgs) Handles ListBoxSaves.Click
+        selectedSave = ListBoxSaves.SelectedItem
+        'get synapsis of that save
+
+        Using sr As StreamReader = File.OpenText(pathlnkynapsisText)
+            Do While sr.Peek() >= 0
+
+                'read the .txt file line by line 
+                savedSynapsis = sr.ReadLine()
+                savedSynapsisText = savedSynapsis
+                'if line is empty then
+                If savedSynapsis.Length < 1 Then
+                Else
+                    'line is not empty
+                    savedSynapsis = savedSynapsis.Substring(0, 3)
+                    If Regex.IsMatch(savedSynapsis, "^[0-9 ]+$") Then
+                        'check if numeric
+                        isnumeric()
+                    Else
+                        savedSynapsis = savedSynapsis.Substring(0, 2)
+                        If Regex.IsMatch(savedSynapsis, "^[0-9 ]+$") Then
+                            If savedSynapsis = selectedSave Then
+                                TextBoxSynapsis.Text = savedSynapsisText
+                            End If
+                        Else
+                            savedSynapsis = savedSynapsis.Substring(0, 1)
+                            If savedSynapsis = selectedSave Then
+                                TextBoxSynapsis.Text = savedSynapsisText
+                            End If
+                        End If
+                    End If
+                End If
+
+            Loop
+        End Using
+
+        If selectedSave = 0 Then
+            TextBoxSynapsis.Text = ""
+        End If
+
+        LabelSTATUS.Text = "Save " & selectedSave & " selected"
+        LabelSTATUS.Left = Me.Width / 2 - LabelSTATUS.Width / 2
+    End Sub
+
+    'CHECK FOR NUMERIC
+    Private Sub isnumeric()
+        If savedSynapsis = selectedSave Then
+            TextBoxSynapsis.Text = savedSynapsisText
+        End If
+    End Sub
+
+    'DELETE
+    Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
+
+
+        saveToDelete = selectedSave
+
+        If selectedSave = Nothing Then
+            MsgBox("Select a SAVE to load")
+        Else
+            'delete folder
+            System.IO.Directory.Delete(pathNoita_RunsFolder & selectedSave, True)
+            'delete synapsis from .txt file
+            Using sr As StreamReader = File.OpenText(pathlnkynapsisText)
+                Do While sr.Peek() >= 0
+
+                    'read the .txt file line by line
+                    saveSynapsisDeleteCandidate = sr.ReadLine()
+                    'if found the right line save it 
+                    If saveSynapsisDeleteCandidate.Contains(saveToDelete) = True Then
+                        Exit Do
+                    End If
+                Loop
+            End Using
+            'get the whole .txt file
+            theWholefile = My.Computer.FileSystem.ReadAllText(pathlnkynapsisText)
+            'delete the line
+            theWholefile = theWholefile.Replace(saveSynapsisDeleteCandidate, "")
+            'delete the old .txt file
+            My.Computer.FileSystem.DeleteFile(pathlnkynapsisText)
+            'save the new .txt file
+            SaveFile = My.Computer.FileSystem.OpenTextFileWriter(pathlnkynapsisText, True)
+            SaveFile.WriteLine(theWholefile)
+            SaveFile.Close()
+
+            checkSaves()
+        End If
+
+        LabelSTATUS.Text = "Deleted!"
+        LabelSTATUS.Left = Me.Width / 2 - LabelSTATUS.Width / 2
+    End Sub
+
+    'OPEN Folder(noita runs)
+    Private Sub ButtonFiles_Click(sender As Object, e As EventArgs) Handles ButtonFiles.Click
+        'open noita runs file location
+        Process.Start("C:\Users\LAdu3L\Desktop\Noita Runs")
+    End Sub
+
+    'Create a shortcut
+    Private Sub CreateShortCut()
+    
+
+        'get target path
+        If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            ShortCutPath = dialog.SelectedPath
+            TargetName = dialog.SelectedPath & "\noita.exe"
+        End If
+
+        Try
+            oShell = CreateObject("WScript.Shell")
+            'what to make shortcut of
+            oLink = oShell.CreateShortcut(ShortCutPath & "\noitaS.lnk")
+            'where to make the shortcut
+            oLink.TargetPath = TargetName
+            oLink.WindowStyle = 1
+            oLink.Save()
+            'create a .txt file that stores the path to noita.exe and the shortcut
+
+
+            SaveFile = My.Computer.FileSystem.OpenTextFileWriter(apppath & "\pathlnk.txt", True)
+            SaveFile.WriteLine(ShortCutPath & "\noitaS.lnk")
+            SaveFile.Close()
+
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+End Class
